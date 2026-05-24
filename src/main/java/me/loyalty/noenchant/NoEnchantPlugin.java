d70e0a2171b8d54f8c33f5bdef8ea325277ce0df
BroVisal <gfjhffb3@gmail.com>
2026-05-24 23:41:01 +0700

    Update NoEnchantPlugin.java

--- src/main/java/me/loyalty/noenchant/NoEnchantPlugin.java
+++ src/main/java/me/loyalty/noenchant/NoEnchantPlugin.java
@@ -1,1 +1,11 @@
+package me.loyalty.noenchant;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NoEnchantPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new EnchantListener(), this);
+    }
+}
