35f7bf204c4d2ae0496d9a4f9a9ac3da019c3a8b
BroVisal <gfjhffb3@gmail.com>
2026-05-25 00:17:18 +0700

    Update NoEnchantPlugin.java

--- src/main/java/me/loyalty/noenchant/NoEnchantPlugin.java
+++ src/main/java/me/loyalty/noenchant/NoEnchantPlugin.java
@@ -1,18 +1,42 @@
 package me.loyalty.noenchant;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NoEnchantPlugin extends JavaPlugin {
 
+    private static NoEnchantPlugin instance;
+
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new EnchantListener(), this);
 
-        getLogger().info("NoEnchantPlugin Enabled!");
+        // Set plugin instance
+        instance = this;
+
+        // Register events
+        Bukkit.getPluginManager().registerEvents(new EnchantListener(), this);
+
+        // Console message
+        getLogger().info("=================================");
+        getLogger().info(" NoEnchantPlugin Enabled!");
+        getLogger().info(" All enchantments are disabled.");
+        getLogger().info(" PaperMC Version: 1.21.x");
+        getLogger().info("=================================");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("NoEnchantPlugin Disabled!");
+
+        // Console message
+        getLogger().info("=================================");
+        getLogger().info(" NoEnchantPlugin Disabled!");
+        getLogger().info("=================================");
+    }
+
+    /*
+     * Get plugin instance
+     */
+    public static NoEnchantPlugin getInstance() {
+        return instance;
     }
 }
