e02a4c3503a287a8fa9eaf6a5bf2288d01649c4a
BroVisal <gfjhffb3@gmail.com>
2026-05-24 23:41:31 +0700

    Update EnchantListener.java

--- src/main/java/me/loyalty/noenchant/EnchantListener.java
+++ src/main/java/me/loyalty/noenchant/EnchantListener.java
@@ -1,1 +1,13 @@
+package me.loyalty.noenchant;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.enchantment.EnchantItemEvent;
+
+public class EnchantListener implements Listener {
+
+    @EventHandler
+    public void onEnchant(EnchantItemEvent event) {
+        event.setCancelled(true);
+    }
+}
