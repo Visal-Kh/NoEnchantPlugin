e7249356bd75859c133d7870e0e8338bf2fc70b1
BroVisal <gfjhffb3@gmail.com>
2026-05-24 23:48:10 +0700

    Update NoEnchantPlugin.java

--- src/main/java/me/loyalty/noenchant/NoEnchantPlugin.java
+++ src/main/java/me/loyalty/noenchant/NoEnchantPlugin.java
@@ -7,5 +7,12 @@ public class NoEnchantPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new EnchantListener(), this);
+
+        getLogger().info("NoEnchantPlugin Enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("NoEnchantPlugin Disabled!");
     }
 }
